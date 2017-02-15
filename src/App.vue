52055e0ecd9ab5a7c40259e545ea684418e3d18c
hgl674420122 <674420122@qq.com>
2017-02-15 17:34:02 +0800

    利用vue 自制简历demo

---
+++ src/App.vue
@@ -0,0 +1,245 @@
+<template>
+  <div id="app">
+    <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
+    <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
+  </div>
+</template>
+
+<script>
+  import StyleEditor from './components/StyleEditor'
+  import ResumeEditor from './components/ResumeEditor'
+  import './assets/reset.css'
+
+  export default {
+    name: 'app',
+    components: {
+      StyleEditor,
+      ResumeEditor
+    },
+    data() {
+      return {
+        interval: 50,
+        currentStyle: '',
+        enableHtml: false,
+        fullStyle: [
+          `/*
+* 尊敬的HR，您好，我叫郝国亮 ，本科毕业，主修计算机专业(数字媒体技术)
+* 二月份了，又一个春暖花开的季节～在这个简历满天飞的时期
+* 我想要找一份web前端开发的工作
+* 言归正传，之前任职于正信(北京)科技股份有限公司，任职web前端开发
+* 年后辞职，希望找到一份适合自己的工作～
+* 说做就做，运用目前主流的vueJs框架我也来写一份简历吧～
+*/
+
+/* 首先给所有元素加上过渡效果 */
+* {
+  -webkit-transition: all .3s;
+  transition: all .3s;
+}
+/* 白色背景太单调了，我们来点背景 */
+html {
+  color: rgb(222,222,222); background: rgb(0,43,54);
+}
+/* 文字离边框太近了 */
+.styleEditor {
+  padding: .5em;
+  border: 1px solid;
+  margin: .5em;
+  overflow: auto;
+  width: 45vw; height: 90vh;
+
+}
+/* 代码高亮 */
+.token.selector{ color: rgb(133,153,0); }
+.token.property{ color: rgb(187,137,0); }
+.token.punctuation{ color: yellow; }
+.token.function{ color: rgb(42,161,152); }
+
+/* 加点 3D 效果呗 */
+html{
+  -webkit-perspective: 1000px;
+          perspective: 1000px;
+}
+.styleEditor {
+  position: fixed; left: 0; top: 0;
+  -webkit-transition: none;
+  transition: none;
+  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
+          transform: rotateY(10deg) translateZ(-100px) ;
+}
+
+/* 接下来我给自己准备一个编辑器 */
+.resumeEditor{
+  position: fixed; right: 0; top: 0;
+  padding: .5em;  margin: .5em;
+  width: 48vw; height: 90vh;
+  border: 1px solid;
+  background: white; color: #222;
+  overflow: auto;
+}
+/* 好了，我开始写简历了 */
+
+
+`,
+          `
+/* 这个简历好像差点什么
+ * 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
+ * 简单，用开源工具翻译成 HTML 就行了
+ */
+`
+          ,
+          `
+/* 再对 HTML 加点样式 */
+.resumeEditor{
+  padding: 2em;
+}
+
+.resumeEditor h2{
+  display: inline-block;
+  border-bottom: 1px solid;
+  margin: 1em 0 .5em;
+}
+.resumeEditor ul,.resumeEditor ol{
+  list-style: none;
+}
+.resumeEditor ul> li::before{
+  content: '•';
+  margin-right: .5em;
+}
+.resumeEditor ol {
+  counter-reset: section;
+}
+.resumeEditor ol li::before {
+  counter-increment: section;
+  content: counters(section, ".") " ";
+  margin-right: .5em;
+}
+.resumeEditor blockquote {
+  margin: 1em;
+  padding: .5em;
+  background: #ddd;
+}
+`],
+        currentMarkdown: '',
+        fullMarkdown: `郝国亮
+----
+
+web前端开发工程师，拥有1.5年工作经验，热爱代码，有代码洁癖，喜欢泡开源社区
+爱好唱歌和爬山 喜欢看球赛～
+
+技能
+----
+
+* 熟练html、css、jquery、javascript、ajax等Web前端开发技术，能够独立完成前端页面，写出符合W3C标准的前端网页代码
+* 熟悉html5和css3特性以及新增的css3标签，熟悉localStorage、sessionStorage本地存储
+* 熟悉主流web前端框架，比如jqmobile、jqueryUI、zepto等前端开发框架；
+* 熟悉bootstrap框架，能够使用swiper、iscroll等插件开发，有快速学习新技术能力
+* 熟悉angularJs，了解MVC设计理念，比如$scope对象使用，数据绑定以及控制器嵌套等
+* 了解vueJs(1.x版本)框架，比如使用webpack脚手架工具、v-model双向绑定以及开启Router
+* 了解php 后台语言以及mysql数据库语言，能够进行页面的前后台交互
+* 熟悉photoshop软件，比如切图、简单美化图片等
+* github、svn 源码托管等
+工作经历
+----
+
+1. 正信科技发展股份有限公司 前端组 *网页制作
+
+2. 智云凌飞科技有限公司  前端组 *网页制作
+
+
+链接
+----
+
+* [GitHub](https://github.com/hgl674420122)
+> 如果你喜欢这个效果，Fork [我的项目](https://github.com/hgl674420122/animating-resume)，打造你自己的简历！
+
+`
+      }
+    },
+    created() {
+      this.makeResume()
+    },
+
+    methods: {
+      makeResume: async function () {
+        await this.progressivelyShowStyle(0)
+        await this.progressivelyShowResume()
+        await this.progressivelyShowStyle(1)
+        await this.showHtml()
+        await this.progressivelyShowStyle(2)
+      },
+      showHtml: function () {
+        return new Promise((resolve, reject) => {
+          this.enableHtml = true
+          resolve()
+        })
+      },
+      progressivelyShowStyle(n) {
+        return new Promise((resolve, reject) => {
+          let interval = this.interval
+          let showStyle = (async function () {
+            let style = this.fullStyle[n]
+            if (!style) { return }
+            // 计算前 n 个 style 的字符总数
+            let length = this.fullStyle.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
+            let prefixLength = length - style.length
+            if (this.currentStyle.length < length) {
+              let l = this.currentStyle.length - prefixLength
+              let char = style.substring(l, l + 1) || ' '
+              this.currentStyle += char
+              if (style.substring(l - 1, l) === '\n' && this.$refs.styleEditor) {
+                this.$nextTick(() => {
+                  this.$refs.styleEditor.goBottom()
+                })
+              }
+              setTimeout(showStyle, interval)
+            } else {
+              resolve()
+            }
+          }).bind(this)
+          showStyle()
+        })
+      },
+      progressivelyShowResume() {
+        return new Promise((resolve, reject) => {
+          let length = this.fullMarkdown.length
+          let interval = this.interval
+          let showResume = () => {
+            if (this.currentMarkdown.length < length) {
+              this.currentMarkdown = this.fullMarkdown.substring(0, this.currentMarkdown.length + 1)
+              let lastChar = this.currentMarkdown[this.currentMarkdown.length - 1]
+              let prevChar = this.currentMarkdown[this.currentMarkdown.length - 2]
+              console.log(prevChar)
+              if (prevChar === '\n' && this.$refs.resumeEditor) {
+                this.$nextTick(() => this.$refs.resumeEditor.goBottom())
+              }
+              setTimeout(showResume, interval)
+            } else {
+              resolve()
+            }
+          }
+          showResume()
+        })
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size:17px;
+  }
+
+  html {
+    min-height: 100vh;
+  }
+
+  * {
+    -webkit-transition: all .3s;
+    transition: all .3s;
+  }
+</style>
